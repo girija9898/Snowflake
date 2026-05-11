ad719aaaadc4f08cdee5e960f3e38bf169c16add
girija9898 <kgirija@defteam.co>
2026-05-11 07:32:18 +0000

    Added subtraction procedure in sample_sql_file

--- Sample_SQL_file.sql
+++ Sample_SQL_file.sql
@@ -14,3 +14,16 @@ END;
 $$;
 
 -- Use this procedure by calling it: CALL add_numbers(5, 10);
+
+CREATE OR REPLACE PROCEDURE subtract_numbers(num1 INTEGER, num2 INTEGER)
+RETURNS INTEGER
+LANGUAGE SQL
+AS
+$$
+DECLARE
+    total INTEGER;
+BEGIN
+    total := num1 - num2;
+    RETURN total;
+END;
+$$;
