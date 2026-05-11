fea422eebf10e8ff789f72f19d8e73ef757b1e02
girija9898 <121597162+girija9898@users.noreply.github.com>
2026-05-11 12:46:22 +0530

    Add files via upload

---
+++ Sample_SQL_file.sql
@@ -0,0 +1,16 @@
+-- Test sample file committed to Snowflake repository
+
+CREATE OR REPLACE PROCEDURE add_numbers(num1 INTEGER, num2 INTEGER)
+RETURNS INTEGER
+LANGUAGE SQL
+AS
+$$
+DECLARE
+    total INTEGER;
+BEGIN
+    total := num1 + num2;
+    RETURN total;
+END;
+$$;
+
+-- Use this procedure by calling it: CALL add_numbers(5, 10);
